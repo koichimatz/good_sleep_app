c80f1861a509df6aa5ed33a3200a0230ff27def0
koichimatz <koichi.matsumoto@gmail.com>
2021-08-26 02:29:20 +0000

    [fix]ルート微修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   resources :sleeps, only: [:new, :create, :index, :show, :destroy]
-
-end
   resources :users, only: [:index, :show, :edit, :update]
+end
+
